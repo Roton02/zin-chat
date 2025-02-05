c28d37bde97e6384b14e89943d094089d7c32cc2
Roton02 <rph645102@gmail.com>
2025-02-05 08:43:03 +0600

    refactor the auth interface and model also validation . add a new filelds is isAcive . by defult is false when a user logged in a then it will update by true .

--- src/module/auth/auth.model.ts
+++ src/module/auth/auth.model.ts
@@ -9,6 +9,7 @@ const userSchema = new Schema<IUser>(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, trim: true },
+    isActive: { type: Boolean, default: false,}
   },
   {
     timestamps: true,
--- src/module/auth/auth.validation.ts
+++ src/module/auth/auth.validation.ts
@@ -1,16 +1,23 @@
-import { z } from "zod";
+import { z } from 'zod'
 
- const registrationValidation = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long").max(50, "Name must be at most 50 characters"),
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(4, "Password must be at least 4 characters long").max(100, "Password is too long"),
-});
- const loginValidation = z.object({
-  email: z.string().email("email is invalid"),
-  password: z.string({required_error: "password is required"})
-});
-
+const registrationValidation = z.object({
+  name: z
+    .string()
+    .min(3, 'Name must be at least 3 characters long')
+    .max(50, 'Name must be at most 50 characters'),
+  email: z.string().email('Invalid email address'),
+  password: z
+    .string()
+    .min(4, 'Password must be at least 4 characters long')
+    .max(100, 'Password is too long'),
+  isActive: z.boolean().default(false),
+})
+const loginValidation = z.object({
+  email: z.string().email('email is invalid'),
+  password: z.string({ required_error: 'password is required' }),
+})
 
 export const userAuthValidation = {
-    registrationValidation,loginValidation
+  registrationValidation,
+  loginValidation,
 }
