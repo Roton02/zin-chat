906f49ed838645bf205aaceda3db6b6108e68ac8
Roton02 <rph645102@gmail.com>
2025-02-06 19:25:43 +0600

    add a new buissness logic in store message service . check the sender and reciver is exist in database

--- src/module/messages/message.services.ts
+++ src/module/messages/message.services.ts
@@ -1,9 +1,27 @@
+import AppError from '../../error/AppError'
 import { user } from '../auth/auth.model'
 import { IMessage } from './message.interface'
 import { messages } from './message.model'
 
 const storeMessageIntroDB = async (payload: IMessage) => {
   //checking the sender and reciver is exist in my database
+  const sender = await user.findById(payload.sender)
+  if (!sender) {
+    throw new AppError(400, 'Sender not found')
+  }
+  const receiver = await user.findById( payload.receiver )
+  if (!receiver) {
+    throw new AppError(400, 'Receiver not found')
+  }
+
+  const isExist = await messages.findOne({
+    sender: sender?._id,
+    receiver: receiver?._id,
+  })
+  if (isExist) {
+    throw new AppError(400, 'Message already exists')
+  }
+
   const result = await messages.create(payload)
   return result
 }
